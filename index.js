3cd9bf820a53b130b4460f31ac8404a9d761c0b5
+ <+>
2024-03-20 15:38:37 +0300

    arithemetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+let num1 =31;
+let num2 =2;
 
-// Write your code here
+let multiply =num1 * num2;  // Multiply num1 and num2
+
+console.log(multiply); // Output: 62
+
+
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+let  num4 = 8; // Define num4
+
+// Calculate num3 such that the remainder of num3 divided by num4 is 4
+let num3 = (4 * num4) + 4;
+
+// Calculate the remainder of dividing num3 by num4
+var mod = num3 % num4;
+
+
+let numbers = [10, 5, 20, 15]; // Define the set of numbers
+
+let max = Math.max(...numbers); // Find the highest number in the set
+
+console.log(max); // Output: 20
